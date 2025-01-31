b72c0c34ed74e725e3a579131e73eb0faf91c8fb
Isis Boelens <ifboelens@tudelft.nl>
2025-01-31 19:33:44 +0100

    Updated project files and removed unnecessary ones

---
+++ mypackage/web_citation_extractor/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 19:27:43 2025
+
+@author: isisboelens
+"""
+
+from .generate_apa_citation import generate_apa_citation
+from .extract_urls_and_citations import extract_urls_and_citations
+from .save_to_csv import save_to_csv
+from .display_metadata_and_save import display_metadata_and_save

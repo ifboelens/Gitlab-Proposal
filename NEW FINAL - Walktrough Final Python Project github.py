2d5a83ff86498203b9a12c03f8dc3b78a6b6b090
Isis Boelens <ifboelens@tudelft.nl>
2025-01-31 15:08:50 +0100

    Proposal of revising my old python project

--- NEW FINAL - Walktrough Final Python Project github.py
+++ NEW FINAL - Walktrough Final Python Project github.py
@@ -6,7 +6,26 @@ Created on Fri Jan 31 14:07:21 2025
 @author: isisboelens
 """
 
+"""
+Since I unsuccesfully used git in my previous submission I will re write my 
+project such that the changes are saved in github.
 
+Previously I used multiple files to track my changes. Each version was a 
+bettered version of the previous one. Usually I sat down to code for a couple 
+of hours and each time I did that I created a new version to make sure 
+that if I made the wrong changes I could look back at my initial work. 
+Exactly what github would've done for me automatically. 
+
+To get a better hang of git functions I will use this file to track my progress
+As my code was good and I am happy with it, I will to re-do what I did
+before using the right tracking methods. This way I can show that I understood 
+the assigment and can work with github.
+
+In addition, I will add testing, which I did not really do previously.
+
+I will keep my old version and work in the folder "Project Citation" so 
+you can see my progress.
+"""
 
 
 
